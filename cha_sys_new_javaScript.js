323baa7f1867c72207b91ccb7d72ce2021e218c8
susgomA <frebaldosandrei@gmail.com>
2023-07-05 23:34:32 +0800

    asdas

--- cha_sys_new_javaScript.js
+++ cha_sys_new_javaScript.js
@@ -1,19 +1,20 @@
 function myFunction() {
   
-
-  var link = document.getElementById("framework").innerHTML;
+  var link = document.getElementById("framework").value;
   
   switch (link) {
-    case 1:
-      document.getElementById("action_page").action = "/cha_sys_new_index.html";
+    case "1":
+      document.getElementById("action_page").action = "cha_sys_new_index.html";
     case "2":
-      document.getElementById("myForm").action = "/action_page.php";
+      document.getElementById("action_page").action = "cha_sys_view_faqs.html";
     case "3":
-      document.getElementById("myForm").action = "/action_page.php";
+      document.getElementById("action_page").action = "/action_page.php";
     case "4":
+      document.getElementById("action_page").action = "/action_page.php";
     case "5":
+      document.getElementById("action_page").action = "/action_page.php";
     case "6":
-
+      document.getElementById("action_page").action = "/action_page.php";
   }
 
 }
