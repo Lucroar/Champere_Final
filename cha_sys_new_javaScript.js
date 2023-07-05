c8bc9c6920912aaba4367d84f983b1022ce2357e
susgomA <frebaldosandrei@gmail.com>
2023-07-05 23:51:43 +0800

    CSS

--- cha_sys_new_javaScript.js
+++ cha_sys_new_javaScript.js
@@ -6,15 +6,15 @@ function myFunction() {
     case "1":
       document.getElementById("action_page").action = "cha_sys_new_index.html";
     case "2":
-      document.getElementById("action_page").action = "cha_sys_view_faqs.html";
+      document.getElementById("action_page").action = "";
     case "3":
-      document.getElementById("action_page").action = "cha_sys_new_index.html";
+      document.getElementById("action_page").action = "";
     case "4":
-      document.getElementById("action_page").action = "cha_sys_new_index.html";
+      document.getElementById("action_page").action = "";
     case "5":
-      document.getElementById("action_page").action = "cha_sys_new_index.html";
+      document.getElementById("action_page").action = "";
     case "6":
-      document.getElementById("action_page").action = "cha_sys_new_index.html";
+      document.getElementById("action_page").action = "";
   }
 
 }
