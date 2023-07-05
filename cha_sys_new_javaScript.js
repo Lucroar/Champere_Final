308ccb58dfb300a1425f7bab0c82a524be9e6677
susgomA <frebaldosandrei@gmail.com>
2023-07-05 23:10:49 +0800

    Case

--- cha_sys_new_javaScript.js
+++ cha_sys_new_javaScript.js
@@ -1,12 +1,16 @@
-function myFunction () {
+
+function myFunction() {
   
 
   var link = document.getElementById("framework").value;
   
   switch (link) {
     case "1":
+      document.getElementById("action_page").action = "cha_sys_new_index.html";
     case "2":
+      document.getElementById("myForm").action = "/action_page.php";
     case "3":
+      document.getElementById("myForm").action = "/action_page.php";
     case "4":
     case "5":
     case "6":
