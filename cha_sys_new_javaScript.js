228478845ad64252f2382326050796ee1df0ecc3
susgomA <frebaldosandrei@gmail.com>
2023-07-06 00:17:22 +0800

    break

--- cha_sys_new_javaScript.js
+++ cha_sys_new_javaScript.js
@@ -3,20 +3,28 @@ function myFunction() {
   var link = document.getElementById("framework").value;
   
   switch (link) {
+    
     case "1":
       document.getElementById("action_page").action = "cha_sys_new_index.html";
+      break;
     case "2":
       document.getElementById("action_page").action = "cha_sys_view_faqs.html";
+      break;
     case "3":
-      document.getElementById("action_page").action = "/action_page.php";
+      document.getElementById("action_page").action = "cha_sys_view_profile.html";
+      break;
     case "4":
-      document.getElementById("action_page").action = "/action_page.php";
+      document.getElementById("action_page").action = "cha_sys_new_about-us.html";
+      break;
     case "5":
-      document.getElementById("action_page").action = "/action_page.php";
+      document.getElementById("action_page").action = "cha_sys_new_customerLogin.html";
+      break;
     case "6":
-      document.getElementById("action_page").action = "/action_page.php";
+      document.getElementById("action_page").action = "cha_sys_new_adminlogin.html";
+      break;
     default:
-      document.getElementById("action_page").action = "cha_sys_new_index.html";
+      document.getElementById("action_page").action = "";
+      break;
   }
 
 }
